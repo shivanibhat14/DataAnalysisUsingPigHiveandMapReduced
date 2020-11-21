925977105fcf856db2f6d3396b157f6e1e48f8de
Shivani Bhat <74504720+shivanibhat14@users.noreply.github.com>
2020-11-21 13:24:48 +0530

    Update CleanData.py

--- CodeSnippets/DataLoadingUsingPig/CleanData.py
+++ CodeSnippets/DataLoadingUsingPig/CleanData.py
@@ -6,8 +6,8 @@ import re
 filename=pd.read_csv("QueryResults.csv")
 data=pd.DataFrame(filename)
 
-data['Body'] = data['Body'].apply(lambda b: re.sub('<.*?>|\\t*\\r*\\n*\\s+',' ',b));
-data['Title'] = data['Title'].apply(lambda t: re.sub('<.*?>|\\t*\\r*\\n*\\s+',' ',t));
+data['Body'] = data['Body'].apply(body: re.sub('<.*?>|\\t*\\r*\\n*\\s+',' ',body));
+data['Title'] = data['Title'].apply(title: re.sub('<.*?>|\\t*\\r*\\n*\\s+',' ',title));
 data.to_csv("QueryResultsClean.csv")
 
 #This code was run for all the csv files and the cleaned data was stored respectively for appending.
